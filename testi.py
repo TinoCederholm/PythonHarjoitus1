234f980a4805f02773520b5d48d6e9852ebb8d34
TinoCederholm <42689.@edu.raseko.fi>
2022-01-11 14:38:38 +0200

    Eka Muutos

---
+++ testi.py
@@ -0,0 +1,12 @@
+# ENSIMMÄINEN HARJOITUS MUUTTUJAT, NÄPPÄIMISTÖ JA NÄYTTÖ
+
+# Määritellään muuttujat
+
+first_name = input("Mikä on etunimesi? ")
+favourite_place = input("Mikä on suosikkipaikkasi? ")
+nickname = input("Mikä on lempinimesi? ")
+year_of_birth = input("Minä vuonna olet syntynyt? ")
+
+# Tulostetaan tiedot ruudulle
+print(first_name, "olet syntynyt", year_of_birth)
+
